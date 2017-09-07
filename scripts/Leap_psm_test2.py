078722a75e4aff51b9239ca349a67424abafd051
cares <careslab@gmail.com>
2017-09-07 15:30:13 -0400

    modified:   Leap_psm_test2.py

--- scripts/Leap_psm_test2.py
+++ scripts/Leap_psm_test2.py
@@ -2,89 +2,78 @@
 # license removed for brevity
 import rospy
 import Leap, sys, thread, time
-from Leap import CircleGesture
 from sensor_msgs.msg._JointState import JointState
 
-class MyListener(Leap.Listener):
-
-    def on_init(self, controller):
-	print "Initialized"
-
-	self.grabstrengthright = 0.0
-	self.grabstrengthleft = 0.0
-
-	self.wristpitchright = 0.0
-	self.wristrollright = 0.0
-	self.wristyawright = 0.0
-	self.outeryaw = 0.0
-	self.outerinsertion = 0.0
-	self.outerpitch = 0.0
-
-	self.wristpitchleft = 0.0
-	self.wristrollleft = 0.0
-	self.wristyawleft = 0.0
-
-    def on_connect(self, controller):
-        print "Connected"
-
-    def on_frame(self, controller):
-        # Get the most recent frame
-        frame = controller.frame()
-	# Get hands
-	for hand in frame.hands:
-	# Get the hand's normal vector and direction
-	    normal = hand.palm_normal
-	    direction = hand.direction
-	    wrist = hand.wrist_position
-
-            if hand.is_right:
-		self.grabstrengthright = 0.0#hand.grab_strength
-		self.wristpitchright = 0.0#direction.pitch
-                self.wristyawright = 0.0#direction.yaw
-                self.wristrollright = 0.0#normal.roll
-		self.outeryaw = wrist[0]
-		self.outerinsertion = wrist[1]
-		self.outerpitch = 0.0#wrist[2]
-
-	    #if hand.is_left:	 
-             #   self.grabstrengthleft = hand.grab_strength
-	      #  self.wristpitchleft = direction.pitch
-		#self.wristpitchleft = 0.0
-                #self.wristyawleft = direction.yaw
-		#self.wristyawleft = 0.0
-                #self.wristrollleft = normal.roll
-		#print 'Wristrollleft %f' %self.wristrollleft
-    	
-    def _repr_(self):
-        return [self.grabstrengthright, self.wristpitchright, self.wristrollright, self.wristyawright, self.outeryaw, self.outerinsertion, self.outerpitch, self.wristpitchleft, self.wristrollleft, self.wristyawleft]
-
 def talker():
-    # Creating a listener and controller
-    mylistener = MyListener()
+    
+    # Creating a controller and initializing parameters
+    grabstrengthright = 0.0
+    wristpitchright = 0.0
+    wristrollright = 0.0
+    wristyawright = 0.0
+    outeryawright = 0.0
+    outerinsertionright = 0.0
+    outerpitchright = 0.0
+    grabstrengthleft = 0.0
+    wristpitchleft = 0.0
+    wristrollleft = 0.0
+    wristyawleft = 0.0
+    outeryawleft = 0.0
+    outerinsertionleft = 0.0
+    outerpitchleft = 0.0
     controller = Leap.Controller()
-    
-    # Have the listener receive events from the controller
-    controller.add_listener(mylistener)
-    
+
+    #Publishers    
     psm1_joint_angles = rospy.Publisher('/dvrk_psm1/joint_states_robot', JointState, queue_size=1)
     psm2_joint_angles = rospy.Publisher('/dvrk_psm2/joint_states_robot', JointState, queue_size=1)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(50) # 50hz
 
+    #JointState Arrays
     msg1 = JointState()
     msg2 = JointState()
 
+    #Do these continuously
     while not rospy.is_shutdown():
 
-	msg1.position = [(mylistener.outeryaw)/100, (30-mylistener.outerpitch)/100, 0.0, 0.0, 0.0, 0.0, 0.0, (250-mylistener.outerinsertion)/1000, mylistener.wristrollright, mylistener.wristpitchright, (mylistener.wristyawright)*2.0, (1.0-mylistener.grabstrengthright)*1.57 , 0.0, 0.0]
+	# Get the most recent frame
+        frame = controller.frame()
+        # Get hands
+        for hand in frame.hands:
+        # Get the hand's normal vector and direction
+	    normal = hand.palm_normal
+	    direction = hand.direction
+	    wrist = hand.wrist_position
+
+	    #Right Hand
+            if hand.is_right:
+	        grabstrengthright = hand.grab_strength
+	        wristpitchright = direction.pitch
+                wristyawright = direction.yaw
+                wristrollright = normal.roll
+	        outeryawright = wrist[0]
+	        outerinsertionright = wrist[1]
+	        outerpitchright = wrist[2]
+	    #Left Hand
+	    if hand.is_left:	 
+                grabstrengthleft = hand.grab_strength
+	        wristpitchleft = direction.pitch
+                wristyawleft = direction.yaw
+                wristrollleft = normal.roll
+		outeryawleft = wrist[0]
+	        outerinsertionleft = wrist[1]
+	        outerpitchleft = wrist[2]
+
+	#Publish the JointStates
+	msg1.position = [(outeryawright)/100.0, (30.0-outerpitchright)/100.0, 0.0, 0.0, 0.0, 0.0, 0.0, (250.0-outerinsertionright)/1000.0, wristrollright, wristpitchright, (wristyawright)*2.0, (1.0-grabstrengthright)*1.57 , 0.0, 0.0]
         msg1.name = ['outer_yaw', 'outer_pitch', 'outer_pitch_1', 'outer_pitch_2', 'outer_pitch_3', 'outer_pitch_4', 'outer_pitch_5', 'outer_insertion', 'outer_roll', 'outer_wrist_pitch', 'outer_wrist_yaw', 'jaw', 'jaw_mimic_1', 'jaw_mimic_2']
-        rospy.loginfo(msg1)
+        #rospy.loginfo(msg1)
         psm1_joint_angles.publish(msg1)
 	
-	#msg2.position = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, mylistener.wristrollleft, mylistener.wristpitchleft, (mylistener.wristyawleft)*2, (1-mylistener.grabstrengthleft)*1.57, 0.0, 0.0]
-        #msg2.name = ['outer_yaw', 'outer_pitch', 'outer_pitch_1', 'outer_pitch_2', 'outer_pitch_3', 'outer_pitch_4', 'outer_pitch_5', 'outer_insertion', 'outer_roll', 'outer_wrist_pitch', 'outer_wrist_yaw', 'jaw', 'jaw_mimic_1', 'jaw_mimic_2']
+	msg2.position = [(outeryawleft)/100.0, (30.0-outerpitchleft)/100.0, 0.0, 0.0, 0.0, 0.0, 0.0, (250.0-outerinsertionleft)/1000.0, wristrollleft, wristpitchleft, (wristyawleft)*2.0, (1-grabstrengthleft)*1.57, 0.0, 0.0]
+        msg2.name = ['outer_yaw', 'outer_pitch', 'outer_pitch_1', 'outer_pitch_2', 'outer_pitch_3', 'outer_pitch_4', 'outer_pitch_5', 'outer_insertion', 'outer_roll', 'outer_wrist_pitch', 'outer_wrist_yaw', 'jaw', 'jaw_mimic_1', 'jaw_mimic_2']
         #rospy.loginfo(msg2)
-        #psm2_joint_angles.publish(msg2)
+        psm2_joint_angles.publish(msg2)
 	
 	rate.sleep()
 
